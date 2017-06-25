5d66b2e26846893ca4f66c4974b27c1e59e80cda
Ashlee Kupor <aekupor@gmail.com>
2017-06-24 21:19:47 -0700

    updated chrome extension

--- chrome-extension/scrape-images.js
+++ chrome-extension/scrape-images.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("Is it working");
+	const selectAllButtons = document.getElementById('scrape-all')
+	console.log(selectAllButtons)
+	selectAllButtons.addEventListener('click', function(event){
+		console.log('button clicked')
+		chrome.tabs.executeScript({
+			code: 'let images = document.querySelectorAll("img[id^=pImage]"); console.log(images)'
+		})
+	})
 });
