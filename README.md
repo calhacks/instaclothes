fefe713586d1ad94084f6e07ee98fc0a813c3fa5
Jacob Young <jacob@jry.io>
2017-06-28 20:32:10 -0500

    Explained the whole app

--- README.md
+++ README.md
@@ -1,9 +1,57 @@
-# InstaFeels
-hi there
-what's up
+## Instafeels - Explained with comments for Ashlee
 
-much documentation
+So the hackathon was a bit crazy right? Especially the second day where I was furiously typing to save the project from disarray.
+I thought I would take the time to write out and explain what happened on that second day so you could understand the final project and
+complete some of the practice we learned together.
 
-please work
+### Overview
 
-thanks Ron
+While the final project wasn't exactly everything we wanted it to be, and had to change the end specification, there are still a lot
+of things to take away from how the final solution works.
+
+The biggest thing there is to learn here, is how **clients and servers interact**. This is huge because it's how we get _dynamic_ data
+on the web.
+
+Ever been to a really old website that was only text/HTML and had no dynamic content (feed, search bar, notifications, etc)?
+That's what the Internet would be like if we couldn't have clients (web browsers that render HTML/CSS/JavaScript) talk to server (written in any language).
+
+The Chrome Extension we worked on now does the following things:
+
+1. (Ashlee) Finds all of the images on a page by **searching the HTML using JavaScript** (`document.querySelectorAll("img[class^='pImage']")`)
+2. (Jacob) Send the links to the images to our server to be processed
+3. (Jacob) Gets a 'response' back from the server with the news images and replaces the old images
+
+### How the Chrome Extension and Server work with each other
+
+This is how the extension and the server interact - meaning the commands they execute, and most importantly in what **order**.
+
+0. **[Extension]** Defines a view to show when the user clicks on the extension button (`main.html`). The view also loads the main script which we use to inject our code into the page (instagram.com)
+1. **[Extension]** Listens for when the "scrape" button in our view is clicked and tells chrome to run the script on the active page we're on
+2. **[Extension]** Our `scrape-images.js` script will get us almost all of the images on the page and return them
+3. **[Extension]** Sends the images from the scraping to the server by calling `sendPostsToServer`. This makes an HTTP Request to the server that's running on the current compute (that's what `localhost` means). It sends the image url, and some data if the comments are happy or sad.
+4. **[Server]** Our server gets the images & the happy/sad values and makes a request to `cloudinary` which is a website that gives us an API to modify the image based on the mood of the comments. It's important to note that Cloudinary API is running on **their own servers**. So servers are computed that exist on the Internet and can talk to each other using HTTP (through the web).
+5. **[Server]** Our server gets a response back from Cloudinary with the new image url. It 'responds' to our Chrome Extension with the following data ` { old: "<the original image url>", new: "<the new image url" }`.
+6. **[Extension]** The extension gets the response back from our server with the old and new images and calls `replace-images.js` which goes back into instagram.com and replaces the original images!
+
+### Things to think about
+
+There's so much information here, the explanations could keep going. But there are a few things I hope you can take away from this project and the code.
+
+The first is an understanding that you can inspect and see how the web works **any time you want**! All you need to do is right-click > "inspect element" on any website just to see how it works!
+
+The second is how most of everything around you **is an API** :P
+As annoying as that word must sound by now, just take a minute to think about it.
+
+For every app, website, and machine you interact with, there is a hidden bundle of code called an API that can be used to do anything the app/website/machine can do!
+Your phone has an API to play sound, take a picture, send a text, etc. Developers can use those APIs to build better and more useful apps for everyone to use.
+They can get two apps that usually never communicate to communicate, like Twitter and your car!
+
+So just know that all of these things are accessible with just a little bit of searching and messing around with code.
+
+
+### Next Steps
+
+Look through the `chrome-extension` directory again, start with `main.js`. I've added comments to all of the files so you can understand what is happening and **why**.
+
+If you have any questions, click on the line number on Github.com and send me the link, I'd be happy to answer anything that comes to mind at all!
+
--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -1,4 +1,51 @@
+/*
+ *                                  HOW WE GOT HERE
+ *
+ * So we're going to explain what's happening with this Chrome Extension and how it works
+ *
+ * A refresheer. To even get a Chrome Extension to work, we need to define a `manifest.json` file.
+ * When we load our extension, this tells Chrome **how our extension is going to work, and what permissions it can have**.
+ *
+ * So we tell chrome that our main file is `main.html`. At the end of `main.html` you'll see this HTML tag:
+ *  <script type="text/javascript" src="main.js"></script>
+ * A <script> tag tells the web page to load some JavaScript. That JavaScript, as you can tell, is this file!
+ *
+ * So that means that when someone clicks on the extension the following things are going to happen:
+ * 1. Chrome is going to display our view (main.html)
+ * 2. It's also going to load our main script file (main.js)
+ *
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Okay now let's explore what this script is doing.
+ *
+ * You'll see a lot this: `(someVariable) => { code }` this is same as a function! `function (someVariable) { code }`
+ *
+ * The fuctions are going to be called in order (A -> B -> C -> and so on), so look down at the bottom
+ * of the file and you'll see where (A) is.
+ *
+ * The functions are in reverse order because in JavaScript we need to define variables/functions before we can use it.
+ * So the order our code is executed from the bottom of the file to the top.
+ *
+ * Note: Not all JavaScript is like this. Most is from the top down, but we're doing a lot of "handling" of events
+ * and other things.
+ * So at the time it made sense to organize the file like this. There are **many** other ways to do it!
+ *
+ */
+
+// =============================================================================
+//                                      (D)
+//                  Updating the website with new images
+// =============================================================================
+//
+// The last step is to provide the oldAndNew data to the `replace-images.js` script
+// Given the old url, it will find the image on the page, and replace it with the new url
 const replaceImage = (data) => {
+  // Just like before we tell chrome to execute a script
+  //
+  // `var data = ...` is a trick to inject our data into the script
+  //
+  // By default all chrome scripts are isolated from each other and can't access
+  // eachother's data.
   chrome.tabs.executeScript({
     code: 'var data = ' + JSON.stringify(data),
   }, function() {
@@ -6,43 +53,72 @@ const replaceImage = (data) => {
       file: 'replace-image.js'
     })
   })
-
-
 }
 
+// =============================================================================
+//                                      (C)
+//                        Making a request to the server
+// =============================================================================
+//
+// Now that the scraper has given us all of the comment and image infromation,
+// we can use it to send the image info to our server.
+//
+// You'll notice that we're using the `$` symbol.
+//
+// This is a "library" called JQuery. It's not an API because it doesn't allow us
+// to do anything we couldn't already do. It's just a collection of functions that are
+// easier to use than the regular ones.
+//
+// In this case, we're using JQuery to make an HTTP request to our server.
+// JQuery uses a term called "AJAX", but two two are the same.
+//
+// With HTTP we want to specify the following into:
+// - WHERE are we getting the information from? The URL
+// - HOW are we fetching information? Are we adding new information? or are we updating existing information?
+// - WHAT are we getting? Do we need to tell the server what we're looking for?
+//
 const sendPostsToServer = (domain, values) => {
   console.log('data', values);
+  // This is just like loop! But we're looping over an array,
+  // and we provide a function for each iteration.
+  // The function gets a new instance every time
   values.forEach(v => {
+    // For the demo we needed to fake these values out
     const url = v.image;
     const happy = Math.floor(Math.random() * 100)
     const sad = Math.floor(Math.random() * 100)
-    console.log('v', v)
-    console.log('happy', happy)
-    console.log('sad', sad)
+    // WHERE = `localhost:300/i`
+    // WHAT = `?url=""&happy=""&sad=""`
     $.ajax(`http://localhost:3000/i?url=${url}&happy=${happy}&sad=${sad}`, {
+      // HOW = GET
       method: 'GET',
     })
     .done(oldAndNew=> {
-      console.log('result', oldAndNew)
+      // {
+      //  old: "url...",
+      //  new: "url...",
+      // }
+      //
+      // Now that we have the old and new image information, we can call our final function
+      // which is going to replace the images and complete the whole processes in (D)
       replaceImage(oldAndNew)
     })
     .fail(error => {
       console.error(error)
     })
   })
-  // const reqPromise = $.ajax(`http://localhost:3000/i?url=${url}&happy=0.99&sad=0.5`, {
-  //   method: 'GET',
-  // })
-  // reqPromise
-  //   .done(oldAndNew=> {
-  //     console.log('result', oldAndNew)
-  //     replaceImage(oldAndNew)
-  //   })
-  //   .fail(error => {
-  //     console.error(error)
-  //   })
 }
 
+// =============================================================================
+//                                      (B)
+//                     Scraping the current page for images
+// =============================================================================
+//
+// This is a function that does one thing: Tell chrome to inject and execute one of
+// our files **into** to the current website.
+//
+// We do this because Chrome Extensions can't automatically read what's on your site.
+// So we have to ask Chrome directly to "executeScript"
 const chromeExecuteScraping = () => {
   // We don't specify a tabId because we want to operate on the current tab
   const tabId = null;
@@ -52,19 +128,40 @@ const chromeExecuteScraping = () => {
   const handleResults = (results) => {
     const result = results[0]
     console.log('RESULT', result)
+
+    // Now that we have the result we want to go to (C)
     sendPostsToServer(result.domain, result.data)
   };
 
   // Inject the script and have chrome return the scraped results
+  // When the scripe is done executing, chrome will call the function we
+  // provide: `handleResult`
   chrome.tabs.executeScript(tabId, options, handleResults);
 };
 
+// =============================================================================
+//                                      (A)
+//                         Setting a button click handler
+// =============================================================================
+//
+// This is the first code that is executed when our extension is clicked
+// A reminder: It looks for our "Scrape All" <button> in `main.html` and sets up a "listener"
+//
+// When that button is clicked, we want to tell chrome to scrape the images from the website
 document.addEventListener('DOMContentLoaded', () => {
-  // Get the button in the chrome popup and bind a click handler to it
-  const scrapeButton = document.getElementById('scrape-all-button')
-  scrapeButton.addEventListener('click', event => {
+
+  const handleScrapeButtonClick = (event) {
     // Tell chrome to inject our scraping script into the page
     chromeExecuteScraping();
-  });
+  }
+
+  // Find the button in `main.html`
+  const scrapeButton = document.getElementById('scrape-all-button')
+
+  // Listen for when the button is clicked. When it's clicked, tell Chrome which function it should call.
+  // I.e. The one we provide to it called `handleScrapeButtonClick`.
+  //
+  // Now that the button has been called we want to go to (B)
+  scrapeButton.addEventListener('click', handleScrapeButtonClick);
 });
 
